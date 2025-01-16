0e073582392ed8a771a56387d6e5bf702881fe47
DarshanaCh <darshanachandarana9@gmail.com>
2025-01-16 17:04:26 +0000

    adding getENV for accessing HEAD varriable from env.prod

--- src/helpers/browsers/browserManager.ts
+++ src/helpers/browsers/browserManager.ts
@@ -1,18 +1,30 @@
-import { chromium, firefox, LaunchOptions } from "playwright";
+import { chromium, firefox, LaunchOptions, Browser } from "playwright";
+import { getEnv } from "../env/env";
 
-const options : LaunchOptions = {
+getEnv();
+// Convert process.env.HEAD to boolean for the headless variable
+const headless: boolean | undefined =
+  process.env.HEAD === "true"
+    ? true
+    : process.env.HEAD === "false"
+    ? false
+    : undefined;
 
-headless: true ,
+// Common launch options
+const options: LaunchOptions = {
+  headless, // Use the converted headless value
+};
 
-}
-export const invokeBrowser = async()  => {
-const browserType = process.env.BROWSER || "chrome"
-    switch(browserType){
-        case "chrome":
-           return chromium.launch(options);
-        case "firefox":
-           return firefox.launch(options);
-         default:
-            throw new Error("Please set the proper browser!") 
-    }
-}
+// Browser Manager Module
+export const invokeBrowser = async () => {
+  const browserType = process.env.BROWSER || "chrome";
+  switch (browserType) {
+    case "chrome":
+      return chromium.launch(options);
+    case "firefox":
+      return firefox.launch(options);
+    default:
+      throw new Error("Please set the proper browser!");
+  }
+};
+
